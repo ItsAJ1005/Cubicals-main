42492475858bb84945716e73ce06751a50c50f0c
ItsAJ1005 <ajharshvardhan123@gmail.com>
2024-09-14 13:10:11 +0530

    Update - package changes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,11 +1,17 @@
 
 import './App.css'
+import { Button } from './components/ui/button'
+import Button2 from './components/ui/button2';
 
 function App() {
 
   return (
-    <div>HI</div>
-  )
+    <div>
+      <div><br/>HI this is the home page</div>
+      <Button2/>  
+    </div>
+
+  );
 }
 
 export default App
