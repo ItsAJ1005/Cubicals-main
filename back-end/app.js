1d76bd6cb90b4d99934d130d4408a5183700e22e
ayush31o5 <127541207+ayush31o5@users.noreply.github.com>
2024-09-20 16:39:18 +0530

    route correction

--- back-end/app.js
+++ back-end/app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
-const db = require("./database/db"); // Import the db instance
+const db = require("./database/db");
 
 class App {
   constructor() {
@@ -12,19 +12,16 @@ class App {
     this.initializeRoutes();
   }
 
-  // Initialize middlewares
   initializeMiddlewares() {
     this.app.use(express.json());
     this.app.use(cookieParser());
     this.app.use(cors());
   }
 
-  // Connect to the database
   connectToDatabase() {
-    db.connect(); // Use the connect method from the db instance
+    db.connect();
   }
 
-  // Initialize routes
   initializeRoutes() {
     const authRoutes = require("./routes/authRoutes");
     const recruiterRoutes = require("./routes/recruiterRoutes");
@@ -34,13 +31,11 @@ class App {
     this.app.use("/recruiter", recruiterRoutes);
     this.app.use("/jobSeeker", jobSeekerRoutes);
 
-    // Base route
     this.app.get("/", (req, res) => {
       res.status(200).json({ message: "Server running" });
     });
   }
 
-  // Start the server
   listen() {
     const PORT = process.env.PORT || 8000;
     this.app.listen(PORT, () => {
@@ -49,6 +44,5 @@ class App {
   }
 }
 
-// Instantiate and run the server
 const appInstance = new App();
 appInstance.listen();
