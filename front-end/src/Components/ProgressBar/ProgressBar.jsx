dc3564013b26738095dfed9f32c53995bcf5c2af
Srikar <161982535+Srikaludev@users.noreply.github.com>
2024-09-21 18:09:10 +0530

    feature/admin-dashboard

---
+++ front-end/src/Components/ProgressBar/ProgressBar.jsx
@@ -0,0 +1,68 @@
+import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
+import KeyboardArrowUpOutlinedIcon from '@mui/icons-material/KeyboardArrowUpOutlined';
+import MoreVertOutlinedIcon from '@mui/icons-material/MoreVertOutlined';
+import { Tooltip } from '@mui/material';
+import React from 'react';
+import 'react-circular-progressbar/dist/styles.css';
+import { Pie, PieChart, ResponsiveContainer } from 'recharts';
+
+import './progressBar.scss';
+
+function ProgressBar() {
+    const data01 = [
+        { name: 'Company1', value: 23 },
+        { name: 'Company2', value: 30 },
+        { name: 'Company3', value: 15 },
+        { name: 'Company4', value: 19 },
+        { name: 'Company5', value: 20 },
+    ];
+
+    return (
+        <div className="progress_bar">
+            <div className="top">
+                <p>Total Job Applications</p>
+                <MoreVertOutlinedIcon />
+            </div>
+
+            <div className="middle">
+                <div className="progress">
+                    <ResponsiveContainer width="100%" height="100%">
+                        <PieChart width={400} height={400}>
+                            <Pie
+                                dataKey="value"
+                                isAnimationActive={false}
+                                data={data01}
+                                cx="50%"
+                                cy="50%"
+                                outerRadius={80}
+                                fill="#536def"
+                                label
+                            />
+                            <Tooltip />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+                <p>Total new Jobs:107</p>
+            </div>
+
+            <div className="bottom">
+                <div className="botom_nested">
+                    <div className="nested_nested">
+                        <p>Last Week</p>
+                        <p className="pricee">
+                            <KeyboardArrowUpOutlinedIcon /> 150
+                        </p>
+                    </div>
+                    <div className="nested_nested">
+                        <p>Last Month</p>
+                        <p className="pricee decrese">
+                            <KeyboardArrowUpOutlinedIcon /> 110
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ProgressBar;
---
+++ front-end/src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,73 @@
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import BarChartIcon from '@mui/icons-material/BarChart';
+import CreditCardIcon from '@mui/icons-material/CreditCard';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LogoutIcon from '@mui/icons-material/Logout';
+import PersonIcon from '@mui/icons-material/Person';
+import SettingsRoundedIcon from '@mui/icons-material/SettingsRounded';
+import TableChartIcon from '@mui/icons-material/TableChart';
+import React, { useContext } from 'react';
+import { Link } from 'react-router-dom';
+import { ColorContext } from '../../ColorContext/darkContext';
+import './Sidebar.scss';
+
+function Sidebar() {
+    const { darkMode, dispatch } = useContext(ColorContext);
+
+    return (
+        <div className="sidebar">
+            <div className="logo">
+                <Link to="/" style={{ textDecoration: 'none' }}>
+                    <h3 className="text_none">Admin Dashboard</h3>
+                </Link>
+            </div>
+
+            <div className="links">
+                <ul>
+                    <p className="spann">Main</p>
+                    <Link to="/" style={{ textDecoration: 'none' }}>
+                        <li>
+                            <DashboardIcon className="icon" /> Dashboard
+                        </li>
+                    </Link>
+
+                    <p className="spann">lists</p>
+                    <Link to="/applicants" style={{ textDecoration: 'none' }}>
+                        <li>
+                            <PersonIcon className="icon" /> Applicant
+                        </li>
+                    </Link>
+                    <Link to="/recruiters" style={{ textDecoration: 'none' }}>
+                        <li>
+                            <PersonIcon className="icon" /> Recruiter
+                        </li>
+                    </Link>
+
+                    <Link to="/Companies" style={{ textDecoration: 'none' }}>
+                        <li>
+                            <TableChartIcon className="icon" /> Companies
+                        </li>
+                    </Link>
+                    <Link to="/JobVacancies" style={{ textDecoration: 'none' }}>
+                        <li>
+                            <CreditCardIcon className="icon" /> Job Vacancies
+                        </li>
+                    </Link>
+
+                    <p className="spann">Seetings</p>
+                    <li>
+                        <AccountCircleIcon className="icon" /> Profile
+                    </li>
+                    <li>
+                        <SettingsRoundedIcon className="icon" /> Setting
+                    </li>
+                    <li>
+                        <LogoutIcon className="icon" /> Log Out
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
