b3e3533dcea0dac54591a44da52783cd29bc91ab
ItsAJ1005 <ajharshvardhan123@gmail.com>
2025-02-19 14:52:11 +0530

    Add - delete company button (Only forntend)

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react'
 import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from '../ui/table'
 import { Avatar, AvatarImage } from '../ui/avatar'
 import { Popover, PopoverContent, PopoverTrigger } from '../ui/popover'
-import { Edit2, MoreHorizontal } from 'lucide-react'
+import { Edit2, MoreHorizontal, Trash2 } from 'lucide-react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import DeleteCompanyDialog from './DeleteCompanyDialog'
 
 const CompaniesTable = () => {
     const { companies, searchCompanyByText } = useSelector(store => store.company);
     const [filterCompany, setFilterCompany] = useState(companies);
+    const [openDeleteDialog, setOpenDeleteDialog ] = useState(false);
     const navigate = useNavigate();
     useEffect(()=>{
         const filteredCompany = companies.length >= 0 && companies.filter((company)=>{
@@ -51,8 +53,22 @@ const CompaniesTable = () => {
                                                 <Edit2 className='w-4' />
                                                 <span>Edit</span>
                                             </div>
+                                            <div
+                                                onClick={() => setOpenDeleteDialog(true)}
+                                                className="flex items-center gap-2 w-fit cursor-pointer text-red-600"
+                                            >
+                                                <Trash2 className="w-4" />
+                                                <span>Delete</span>
+                                            </div>
+                                            
                                         </PopoverContent>
                                     </Popover>
+
+                                    <DeleteCompanyDialog
+                                        open={openDeleteDialog}
+                                        setOpen={setOpenDeleteDialog}
+                                        companyId={company._id}
+                                    />  
                                 </TableCell>
                             </tr>
 
